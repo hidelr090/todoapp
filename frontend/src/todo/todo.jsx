208928022a4e0f34be3cbbdb6d10d503d1101a27
hidelr090 <hidelgardo503@gmail.com>
2021-02-25 15:27:58 -0300

    get and delete todos

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -15,21 +15,27 @@ export default class Todo extends Component{
         this.handleAdd = this.handleAdd.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.refresh = this.refresh.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
     }
 
     refresh(){
         axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => console.log(resp.data));
+            .then(resp => this.setState({...this.state, description: '', list:resp.data}));
     }
 
     handleAdd(){
         const description = (this.state.description);
         axios.post(URL, {description})
-            .then(resp => console.log('Funcionou'));
+            .then(resp => this.refresh());
     }
 
     handleChange(e){
         this.setState({...this.state, description:e.target.value});
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh());
     }
 
     render(){
@@ -37,7 +43,7 @@ export default class Todo extends Component{
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
                 <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange}/>
-                <TodoList/>
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
             </div>
         );
     }
