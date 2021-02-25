20da1262afdac15857636467f7178ae1fc709195
hidelr090 <hidelgardo503@gmail.com>
2021-02-25 10:47:28 -0300

    conditional render, add event, onchange event and add event backend integration

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -3,12 +3,34 @@ import PageHeader from '../template/pageheader';
 import TodoForm from './todoform';
 import TodoList from './todolist';
 
+import axios from 'axios';
+
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {description: '', list: []};
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleAdd(){
+        const description = (this.state.description);
+        axios.post(URL, {description})
+            .then(resp => console.log('Funcionou'));
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description:e.target.value});
+    }
+
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
-                <TodoForm/>
+                <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange}/>
                 <TodoList/>
             </div>
         );
--- frontend/src/todo/todoform.jsx
+++ frontend/src/todo/todoform.jsx
@@ -5,11 +5,11 @@ import IconButton from '../template/iconbutton';
 export default props =>(
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'/>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa' input={props.description} onChange={props.handleChange}/>
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}>
                 <i className='fa fa-plus'></i>
             </IconButton>
         </Grid>
