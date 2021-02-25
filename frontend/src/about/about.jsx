69223a107e20fcac3b50c52cb72a5f730ac515bd
hidelr090 <hidelgardo503@gmail.com>
2021-02-24 22:59:20 -0300

    creating navbar menu, routes and pageheader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PageHeader from '../template/pageheader';
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas officiis commodi, culpa corrupti dolor repudiandae eos quaerat, impedit neque nulla deleniti nostrum eum vel? Sequi sunt quae consequatur unde illum?</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt sit architecto dignissimos, voluptatem accusantium iure minus fuga dolore fugiat velit porro, earum repellat ab aperiam hic officia voluptates blanditiis. Eaque.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae odit rem deserunt eligendi. Sit, molestias odio, nulla culpa aperiam sed quibusdam nisi facilis obcaecati officiis architecto a nam vel.</p>
     </div>
 );
